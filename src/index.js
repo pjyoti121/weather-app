622f2779a54a74f7a3859fb0ac6cbb79f886e6d0
=pjyoti121 <jyotipandey20.setmodyuniversity.ac.in>
2022-08-14 21:56:15 +0530

    weatherapp

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {WeatherProvider} from "./context/Weather";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeatherProvider>
+      <App/>
+    </WeatherProvider>
+    
   </React.StrictMode>
 );
 
